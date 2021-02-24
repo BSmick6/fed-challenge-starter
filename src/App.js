c376011c3acc830c734bb38f6812b5ddd2469d4c
Brock Smickley <bsmick97@gmail.com>
2021-02-24 14:10:18 -0500

    initial cleanup and images added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <img src="images\lake-inniscarra-thumb.jpg" alt="" />
+      </div>
+      <div className="card">
+        <img src="images\performance-series-thumb.jpg" alt="" />
+      </div>
+      <div className="card">
+        <img src="images\slow-pulls-thumb.jpg" alt="" />
+      </div>
+      <div className="card">
+        <img src="images\20-minutes-to-toned-thumb.jpg" alt="" />
+      </div>
+      <div className="card">
+        <img src="images\charles-race-thumb.jpg" alt="" />
+      </div>
+      <div className="card">
+        <img src="images\full-body-hiit-thumb.jpg" alt="" />
+      </div>
+      <div className="card">
+        <img src="images\kafue-river-thumb.jpg" alt="" />
+      </div>
+      <div className="card">
+        <img src="images\shred-and-burn-thumb.jpg" alt="" />
+      </div>
     </div>
   );
 }
